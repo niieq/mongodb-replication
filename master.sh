c412ec18d8c3884a263942b2d71a6f737e97ed23
Nii Equity <nii.nmarley@gmail.com>
2017-01-10 19:20:07 +0000

    Master slave Provisioning cleanup

--- master.sh
+++ master.sh
@@ -8,6 +8,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
 apt-get -y install mongodb-10gen
+mkdir -p /data/db
 
 service mongodb stop
 cp -f /vagrant/mastercfg.cnf /etc/mongodb.conf
@@ -25,4 +26,12 @@ sleep 10
 mongo 192.168.33.11 <<EOF
 rs.add("slave")
 rs.conf()
+EOF
+
+echo "Inserting Sample data"
+mongo 192.168.33.11 <<EOF
+use students
+db.students.insert({name: "stud1"})
+db.students.insert({name: "stud2"})
+db.students.insert({name: "stud3"})
 EOF
